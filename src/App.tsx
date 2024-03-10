032477748a1cf5ea65d6c2792a56c8d34d4d0126
Timofey <pttima@yandex.ru>
2024-03-10 19:29:21 +0300

    routing via react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 
 import './App.css'
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import DefaultPage from './components/Pages/DefaultPage'
+import UserPage from './components/Pages/UserPage'
+import TodosPage from './components/Pages/TodosPage'
+import { Link } from 'react-router-dom'
 
 const App = () => {
 
   return (
     <BrowserRouter>
       <div className="root">
-
+        <div>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/users'}>Users</Link>
+          <Link to={'/todos'}>Todos</Link>
+        </div>
+        <Routes>
+          <Route path='/' element={<DefaultPage />} />
+          <Route path='/users' element={<UserPage />} />
+          <Route path='/todos' element={<TodosPage />} />
+        </Routes>
       </div>
     </BrowserRouter>
   )
